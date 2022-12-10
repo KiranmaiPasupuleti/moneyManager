f9b1706ace89de359683a000e83b25dede1fba84
KiranmaiPasupuleti <pasupuletikiranmai1@gmail.com>
2022-12-10 13:09:33 +0000

    first commit

--- src/components/MoneyManager/index.js
+++ src/components/MoneyManager/index.js
@@ -124,7 +124,7 @@ class MoneyManager extends Component {
       <div className="app-container">
         <div className="responsive-container">
           <div className="header-container">
-            <h1 className="heading">Hi, Shankar</h1>
+            <h1 className="heading">Hi, Kiranmai</h1>
             <p className="header-content">
               Welcome back to your
               <span className="money-manager-text"> Money Manager</span>
